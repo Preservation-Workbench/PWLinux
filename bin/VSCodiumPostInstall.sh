2425d63ae652ad68aafc55d9e8531ed1dc14dddc
Morten Eek <mortenee@gmail.com>
2019-09-05 11:30:11 +0200

    VSCode script fix

--- bin/VSCodiumPostInstall.sh
+++ bin/VSCodiumPostInstall.sh
@@ -8,7 +8,7 @@ sudo -H -u $OWNER bash -c 'codium --install-extension ms-python.python';
 
 VS_DIR="/home/$OWNER/.config/VSCodium/User/.git/"
 if [ ! -d "$VS_DIR" ]; then
-    sudo -H -u $OWNER bash -c "https://github.com/BBATools/VSCodiumSettings.git /home/$OWNER/.config/VSCodium/User/";
+    sudo -H -u $OWNER bash -c "git clone https://github.com/BBATools/VSCodiumSettings.git /home/$OWNER/.config/VSCodium/User/";
 fi
 
 #xdg-mime default codium.desktop text/english text/plain text/x-makefile text/x-c++hdr text/x-c++src text/x-chdr text/x-csrc text/x-java text/x-moc text/x-pascal text/x-tcl text/x-tex application/x-shellscript text/x-c text/x-c++
