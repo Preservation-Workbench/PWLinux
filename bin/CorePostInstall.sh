0799b3b574eabb3a41363ec56dd87e3652a67447
Morten Eek <mortenee@gmail.com>
2019-09-05 15:37:23 +0200

    bash fix

--- bin/CorePostInstall.sh
+++ bin/CorePostInstall.sh
@@ -12,4 +12,19 @@ sudo -H -u $OWNER bash -c "python3 -m pip install -U ttkthemes unoconv execsql e
 wget -qO /tmp/ripgrep.deb https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb;
 apt-get install -y /tmp/ripgrep.deb;
 
+cat <<\EOF > /home/$OWNER/bin/div_fix.sh
+#! /bin/bash
+pkill gvfsd-fuse && /usr/lib/gvfs/gvfsd-fuse -o allow_other /var/run/user/1000/gvfs/
+setxkbmap -option 'numpad:microsoft'
+#"$ORACLE_HOME"/bin/lsnrctl start && sudo /etc/init.d/oracle-xe start
+EOF
+chmod a+rx /home/$OWNER/bin/div_fix.sh
+
+echo "[Desktop Entry]
+Type=Application
+Exec=/home/bba/bin/div_fix.sh
+Hidden=false
+X-MATE-Autostart-enabled=true
+Name=Div_Fix" > /home/$OWNER/.config/autostart/Div_Fix.desktop
+
 
