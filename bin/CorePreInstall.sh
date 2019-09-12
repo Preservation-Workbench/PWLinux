c47b3a8e518c540ac3bc1012b4df860d83b9db7b
Morten Eek <mortenee@gmail.com>
2019-09-12 15:05:22 +0200

    Run mintupdate first

--- bin/CorePreInstall.sh
+++ bin/CorePreInstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo mintupdate-cli -y --keep-configuration upgrade;
+apt autoremove;
 sudo apt-get remove --purge `dpkg -l | grep '^rc' | awk '{print $2}'` #Remove residual config
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections;
 
