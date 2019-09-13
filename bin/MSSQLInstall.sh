74d24dddbeeb8ab9278ef4f5399c294aebaf63cd
Morten Eek <mortenee@gmail.com>
2019-09-13 12:49:04 +0200

    mssql bug workaround

--- bin/MSSQLInstall.sh
+++ bin/MSSQLInstall.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# TODO: Aktiver installasjon fra repo heller når feil med korrupt fil etter restart fikset av MS (er feil i versjon 15.0.1900.25-1)
 if [ $(dpkg-query -W -f='${Status}' mssql-server 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | apt-key add -;
-    add-apt-repository "deb https://packages.microsoft.com/ubuntu/16.04/mssql-server-preview/ xenial main" # TODO: Endre repo når tilgjengelig for 18.04
-    apt-get update;
-    apt-get install -y mssql-server;
+    #wget -qO- https://packages.microsoft.com/keys/microsoft.asc | apt-key add -;
+    #add-apt-repository "deb https://packages.microsoft.com/ubuntu/16.04/mssql-server-preview/ xenial main" # TODO: Endre repo når tilgjengelig for 18.04
+    #apt-get update;
+    #apt-get install -y mssql-server;
+    cd /tmp && wget -c https://packages.microsoft.com/ubuntu/16.04/mssql-server-preview/pool/main/m/mssql-server/mssql-server_15.0.1800.32-1_amd64.deb
+    sudo DEBIAN_FRONTEND=noninteractive dpkg --install mssql-server_15.0.1800.32-1_amd64.deb
 fi
 
 export ACCEPT_EULA="Y"
