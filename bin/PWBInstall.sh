7397e7e806193b807638f7fdbcff962b56a18af6
Morten Eek <mortenee@gmail.com>
2019-09-13 13:17:27 +0200

    ojdbc install

--- bin/PWBInstall.sh
+++ bin/PWBInstall.sh
@@ -3,7 +3,7 @@
 apt install -y wimtools python3-pandas graphviz python3-lxml python3-tk openjdk-11-jdk;
 
 
-# TODO: tmp-mappe blir liggende igjen ved andre gangs kjøring -> gjelder sikkert også andre -> fix
+# TODO: Legg inn dokumentasjon på at ojdbc10.jar må lastes ned manuelt og legges i bin-katalog
 
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
@@ -13,13 +13,19 @@ REPOSRC="https://github.com/BBATools/PreservationWorkbench.git"
 LOCALREPO="/home/$OWNER/bin/PWB"
 sudo -H -u $OWNER bash -c "git clone "$REPOSRC" "$LOCALREPO" 2> /dev/null || git -C "$LOCALREPO" pull"
 
-if [ ! -f $PWB_DIR/bin/sqlworkbench.jar ]; then
+if [ ! -f $LOCALREPO/bin/sqlworkbench.jar ]; then
     sudo -H -u $OWNER bash -c "cd /home/$OWNER/bin/PWB/bin && \
     wget -qO tmp.zip https://www.sql-workbench.eu/Workbench-Build125.zip && unzip -o tmp.zip && rm tmp.zip; \
     ln -s /home/$OWNER/bin/PWB/bin/pwb.sh /home/$OWNER/bin; \
     sed -i 's:java -jar sqlworkbench.jar:/home/$OWNER/bin/PWB/bin/pwb.sh:g' sqlworkbench.desktop; \
     sed -i 's:workbench32.png:/home/$OWNER/bin/PWB/bin/workbench32.png:g' sqlworkbench.desktop; \
     cp sqlworkbench.desktop /home/$OWNER/.local/share/applications/";
+fi
+
+if [ ! -f $LOCALREPO/bin/ojdbc10.jar ]; then
+    if [ -f $SCRIPTPATH/ojdbc10.jar ]; then
+        sudo -H -u $OWNER bash -c "cp $SCRIPTPATH/ojdbc10.jar $LOCALREPO/bin/";
+    fi
 fi
 
 
