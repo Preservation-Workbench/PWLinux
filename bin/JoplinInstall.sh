212eba2a955af90a0a19acac66d3e1f5087612e9
Morten Eek <mortenee@gmail.com>
2019-09-18 14:43:53 +0200

    Fixed tput error

--- bin/JoplinInstall.sh
+++ bin/JoplinInstall.sh
@@ -3,7 +3,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 OWNER=$(stat -c '%U' $SCRIPTPATH);
 
-# TODO: Fix FM: tput no value for $term
-sudo -H -u $OWNER bash -c "wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash"
+sudo -H -u $OWNER bash -c "export TERM=xterm ; wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash"
 
 
