2eb93e78960675568642654b1c087ced612c6b13
Morten Eek <mortenee@gmail.com>
2019-09-19 11:08:54 +0200

    Joplin fix

--- bin/JoplinInstall.sh
+++ bin/JoplinInstall.sh
@@ -3,6 +3,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 OWNER=$(stat -c '%U' $SCRIPTPATH);
 
-sudo -H -u $OWNER bash -c "export TERM=xterm ; wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash"
+sudo -H -u $OWNER bash -c "export XDG_CURRENT_DESKTOP=XFCE ; export TERM=xterm ; wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash"
 
 
