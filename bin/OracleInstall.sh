e36580a4169631408fe0279daa47c4671d247649
Morten Eek <mortenee@gmail.com>
2019-09-23 12:47:32 +0200

    Added licence file. Oracle install fix

--- bin/OracleInstall.sh
+++ bin/OracleInstall.sh
@@ -78,7 +78,7 @@ SQL"
 rm -rdf /home/$OWNER/oradiag_root;
 
 #Fix desktop icons:
-rm /home/$OWNER/Desktop/oraclexe-gettingstarted.desktop
+rm -f /home/$OWNER/Desktop/oraclexe-gettingstarted.desktop
 sed -zi '/NoDisplay=true/!s/$/\nNoDisplay=true/' /usr/share/applications/oraclexe-getstarted.desktop
 sed -zi '/NoDisplay=true/!s/$/\nNoDisplay=true/' /usr/share/applications/oraclexe-gettingstarted.desktop
 sed -zi '/NoDisplay=true/!s/$/\nNoDisplay=true/' /usr/share/applications/oraclexe-backup.desktop
