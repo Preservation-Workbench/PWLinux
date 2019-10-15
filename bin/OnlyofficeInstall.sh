155d15d916b44e509fd446f9c09d0e3b1fee4e4d
Morten Eek <mortenee@gmail.com>
2019-10-15 13:55:28 +0200

    Cleanup

--- bin/OnlyofficeInstall.sh
+++ bin/OnlyofficeInstall.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 if [ $(dpkg-query -W -f='${Status}' onlyoffice-desktopeditors 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    #wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -;
     curl -sSL 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xe09ca29f6e178040ef22b4098320ca65cb2de8e5' | sudo apt-key add -;
     echo "deb https://download.onlyoffice.com/repo/debian squeeze main" > /etc/apt/sources.list;
     apt-get update;
     apt-get install -y onlyoffice-desktopeditors;
 fi
 
-
-#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
