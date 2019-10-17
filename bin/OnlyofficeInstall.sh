1bb3f2b185977a8269d26f2dacfb8fd8cf17b947
Morten Eek <mortenee@gmail.com>
2019-10-17 11:14:21 +0200

    Fix repo bugs

--- bin/OnlyofficeInstall.sh
+++ bin/OnlyofficeInstall.sh
@@ -2,7 +2,7 @@
 
 if [ $(dpkg-query -W -f='${Status}' onlyoffice-desktopeditors 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     curl -sSL 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xe09ca29f6e178040ef22b4098320ca65cb2de8e5' | sudo apt-key add -;
-    echo "deb https://download.onlyoffice.com/repo/debian squeeze main" > /etc/apt/sources.list;
+    echo "deb https://download.onlyoffice.com/repo/debian squeeze main" >> /etc/apt/sources.list;
     apt-get update;
     apt-get install -y onlyoffice-desktopeditors;
 fi
