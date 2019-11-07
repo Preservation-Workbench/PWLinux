69fb7379a3cd897f8614953c88d8f4acb1e1bb9a
Morten Eek <mortenee@gmail.com>
2019-11-07 14:17:41 +0100

    Added verapdf

--- bin/PWBInstall.sh
+++ bin/PWBInstall.sh
@@ -57,3 +57,34 @@ if [ -f "/etc/ImageMagick-6/policy.xml" ]; then
     mv /etc/ImageMagick-6/policy.xml /etc/ImageMagick-6/policy.xmlout  2>/dev/null;
 fi
 
+
+# WAIT: Lag menyvalg + kopier eller lenke executables til path
+#VERAPDF:
+sudo -H -u $OWNER bash -c '
+mkdir -p /home/$OWNER/bin/verapdf
+cd /tmp && wget http://downloads.verapdf.org/rel/verapdf-installer.zip
+unzip verapdf-installer.zip
+cd verapdf-greenfield-*
+cat <<\EOF > auto.xml
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<AutomatedInstallation langpack="eng">
+<com.izforge.izpack.panels.htmlhello.HTMLHelloPanel id="welcome"/>
+<com.izforge.izpack.panels.target.TargetPanel id="install_dir">
+<installpath>/home/bba/bin/verapdf</installpath>
+</com.izforge.izpack.panels.target.TargetPanel>
+<com.izforge.izpack.panels.packs.PacksPanel id="sdk_pack_select">
+<pack index="0" name="veraPDF GUI" selected="true"/>
+<pack index="1" name="veraPDF Mac and *nix Scripts" selected="true"/>
+<pack index="2" name="veraPDF Corpus and Validation model" selected="false"/>
+<pack index="3" name="veraPDF Documentation" selected="true"/>
+<pack index="4" name="veraPDF Sample Plugins" selected="false"/>
+</com.izforge.izpack.panels.packs.PacksPanel>
+<com.izforge.izpack.panels.install.InstallPanel id="install"/>
+<com.izforge.izpack.panels.finish.FinishPanel id="finish"/>
+</AutomatedInstallation>
+EOF
+java -jar verapdf-izpack-installer-* auto.xml'
+
+
+
+
