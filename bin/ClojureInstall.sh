035e01544822515249d48aa2ba7a0cc1f6a0c2b1
Morten Eek <mortenee@gmail.com>
2019-11-18 10:03:05 +0100

    Use curl doh

--- bin/ClojureInstall.sh
+++ bin/ClojureInstall.sh
@@ -5,14 +5,9 @@ OWNER=$(stat -c '%U' $SCRIPTPATH);
 lein_path="/home/$OWNER/bin/lein"
 
 if [ ! -f $lein_path ]; then
-    wget -qO $lein_path https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein;
+    curl -o $lein_path --doh-url https://1.1.1.1/dns-query https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein;
     chmod a+x $lein_path;
     chown $OWNER:$OWNER $lein_path;
     sudo -H -u $OWNER bash -c "$lein_path";
 fi
-
-
-
-
-
 
