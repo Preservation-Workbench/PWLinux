798456e9e4105f51917e05bf29b5ba46590298ad
Morten Eek <mortenee@gmail.com>
2020-01-13 15:54:16 +0100

    Add mssql-tools

--- bin/MSSQLInstall.sh
+++ bin/MSSQLInstall.sh
@@ -5,9 +5,11 @@ sudo apt install libc++1 libc++abi1 libsasl2-modules-gssapi-mit libsss-nss-idmap
 # TODO: Aktiver installasjon fra repo heller når feil med korrupt fil etter restart fikset av MS (er feil i versjon 15.0.1900.25-1)
 if [ $(dpkg-query -W -f='${Status}' mssql-server 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | apt-key add -;
-    add-apt-repository "deb https://packages.microsoft.com/ubuntu/16.04/mssql-server-preview/ xenial main" # TODO: Endre repo når tilgjengelig for 18.04
+    add-apt-repository "deb https://packages.microsoft.com/ubuntu/16.04/mssql-server-preview/ xenial main"; # TODO: Endre repo når tilgjengelig for 18.04
+    add-apt-repository "deb https://packages.microsoft.com/ubuntu/18.04/prod bionic main"; 
     apt-get update;
     apt-get install -y mssql-server;
+    ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev;
     #cd /tmp && wget -c https://packages.microsoft.com/ubuntu/16.04/mssql-server-preview/pool/main/m/mssql-server/mssql-server_15.0.1800.32-1_amd64.deb
     #sudo DEBIAN_FRONTEND=noninteractive dpkg --install mssql-server_15.0.1800.32-1_amd64.deb
 fi
