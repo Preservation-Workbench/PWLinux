c53e5d4a48570cdbe46b0aea53649dc2f87fe943
Morten Eek <mortenee@gmail.com>
2020-10-23 14:55:04 +0200

    Removed freeoffice

--- bin/FreeofficeInstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $(dpkg-query -W -f='${Status}' softmaker-freeoffice-2018 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    wget --quiet -O - http://shop.softmaker.com/repo/linux-repo-public.key | apt-key add -;
-    echo "deb https://shop.softmaker.com/repo/apt wheezy non-free" >> /etc/apt/sources.list;
-    apt-get update;
-    apt-get install -y softmaker-freeoffice-2018;
-fi
-
