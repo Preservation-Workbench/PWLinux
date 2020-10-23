7408354adbaabed4c2c66ef9447a323a0be7f196
Morten Eek <mortenee@gmail.com>
2020-10-23 14:57:12 +0200

    Improved Foliate install

--- bin/FoliateInstall.sh
+++ bin/FoliateInstall.sh
@@ -3,5 +3,4 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 OWNER=$(stat -c '%U' $SCRIPTPATH)
 
-sudo apt-get install flatpak;
 sudo -H -u $OWNER bash -c "flatpak install -y flathub com.github.johnfactotum.Foliate";
