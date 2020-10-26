8f33fac4a5782d50f3c6e5e38ba327b266a13bf7
Morten Eek <mortenee@gmail.com>
2020-10-26 15:01:32 +0100

    Use apt for spotify install

--- bin/SpotifyInstall.sh
+++ bin/SpotifyInstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-OWNER=$(stat -c '%U' $SCRIPTPATH)
-
-sudo -H -u $OWNER bash -c "flatpak install -y flathub com.spotify.Client";
+if [ $(dpkg-query -W -f='${Status}' spotify 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    echo deb http://repository.spotify.comstable non-free | sudo tee /etc/apt/sources.list.d/spotify.list;
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4773BD5E130D1D45;
+    apt-get update;
+    sudo apt install -y spotify-client;
+fi
