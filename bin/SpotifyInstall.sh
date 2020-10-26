c984f39a04e040baad7fc8eeaaaef87648379c92
Morten Eek <mortenee@gmail.com>
2020-10-26 15:20:59 +0100

    Spotify fix

--- bin/SpotifyInstall.sh
+++ bin/SpotifyInstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [ $(dpkg-query -W -f='${Status}' spotify 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    echo deb http://repository.spotify.comstable non-free | sudo tee /etc/apt/sources.list.d/spotify.list;
+    echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list;
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4773BD5E130D1D45;
     apt-get update;
-    sudo apt install -y spotify-client;
+    apt-get install -y spotify-client;
 fi
+
