ee14bb4cc97ae528a1ab342ced9d99d4eab2d08b
Morten Eek <mortenee@gmail.com>
2020-10-26 14:22:39 +0100

    WPSOffice install fix

--- bin/WPSOfficeInstall.sh
+++ bin/WPSOfficeInstall.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-OWNER=$(stat -c '%U' $SCRIPTPATH)
-
-sudo -H -u $OWNER bash -c "flatpak install -y flathub com.wps.Office";
+flatpak install -y flathub com.wps.Office
