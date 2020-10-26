397667055edbc8b80205dcec4cae97d673ee41c9
Morten Eek <mortenee@gmail.com>
2020-10-26 14:05:53 +0100

    mscorefonts auto install

--- bin/OnlyofficeInstall.sh
+++ bin/OnlyofficeInstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections;
+
 isInFile=$(cat /etc/apt/sources.list | grep -c "https://download.onlyoffice.com/repo/debian")
 if [ $isInFile -eq 0 ]; then
     curl -sSL 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xe09ca29f6e178040ef22b4098320ca65cb2de8e5' | sudo apt-key add -;
