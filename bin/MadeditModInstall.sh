dc8fecd6e9137f26c2c9cf18344ae999edd81c82
Morten Eek <mortenee@gmail.com>
2020-10-28 12:33:12 +0100

    Updated Madedit version

--- bin/MadeditModInstall.sh
+++ bin/MadeditModInstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ $(dpkg-query -W -f='${Status}' madedit-mod 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    wget -qO /tmp/package_dep.deb https://sourceforge.net/projects/madedit-mod/files/0.4.17/madedit-mod_0.4.17-1_amd64_ubuntu%2018.04.deb/download;
+    wget -qO /tmp/package_dep.deb https://sourceforge.net/projects/madedit-mod/files/0.4.19/madedit-mod_0.4.19-1_amd64_Ubuntu19.10.deb/download;
     apt-get install -y /tmp/package_dep.deb;
 fi
-
