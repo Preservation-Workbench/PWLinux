5709bd27646ce3266f1f98e7799c8c1446898ddc
Morten Eek <mortenee@gmail.com>
2020-10-28 12:10:57 +0100

    Added vscode default config

--- bin/VSCodeInstall.sh
+++ bin/VSCodeInstall.sh
@@ -13,16 +13,13 @@ fi
 
 apt-get install -y code;
 
+sudo -H -u $OWNER bash -c "code --install-extension smlombardi.soho; \
+code --install-extension fabiospampinato.vscode-highlight; \
+code --install-extension ms-python.python;";
 
-
-# sudo -H -u $OWNER bash -c "codium --install-extension smlombardi.soho; \
-# codium --install-extension fabiospampinato.vscode-highlight; \
-# codium --install-extension ms-python.python;";
-
-# REPOSRC="https://github.com/BBATools/VSCodiumSettings.git"
-# LOCALREPO="/home/$OWNER/.config/VSCodium/User"
-# sudo -H -u $OWNER bash -c "git clone "$REPOSRC" "$LOCALREPO" 2> /dev/null || git -C "$LOCALREPO" pull"
+REPOSRC="https://github.com/Preservation-Workbench/vscode_config.git"
+LOCALREPO="/home/$OWNER/.config/Code/User"
+sudo -H -u $OWNER bash -c "git clone "$REPOSRC" "$LOCALREPO" 2> /dev/null || git -C "$LOCALREPO" pull"
 
 #xdg-mime default codium.desktop text/english text/plain text/x-makefile text/x-c++hdr text/x-c++src text/x-chdr text/x-csrc text/x-java text/x-moc text/x-pascal text/x-tcl text/x-tex application/x-shellscript text/x-c text/x-c++
-
 
