98d0132938709a65e35dc7927874c92a14d0ce3b
Morten Eek <mortenee@gmail.com>
2020-10-28 10:32:34 +0100

    Fixes. Removed Typora

--- bin/OnlyofficeInstall.sh
+++ bin/OnlyofficeInstall.sh
@@ -4,11 +4,11 @@ echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select tr
 
 isInFile=$(cat /etc/apt/sources.list | grep -c "https://download.onlyoffice.com/repo/debian")
 if [ $isInFile -eq 0 ]; then
-    curl -sSL 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xe09ca29f6e178040ef22b4098320ca65cb2de8e5' | sudo apt-key add -;
+    curl -sSL 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xe09ca29f6e178040ef22b4098320ca65cb2de8e5' | apt-key add -;
     echo "deb https://download.onlyoffice.com/repo/debian squeeze main" >> /etc/apt/sources.list;
     apt-get update;
 fi
 
-apt install -y onlyoffice-desktopeditors;
+apt-get install -y onlyoffice-desktopeditors;
 
 
