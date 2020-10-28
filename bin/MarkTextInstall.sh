b96933a03c3629627c644d9310e57115afdcfd6c
Morten Eek <mortenee@gmail.com>
2020-10-28 10:11:30 +0100

    Improved MarkText install

--- bin/MarkTextInstall.sh
+++ bin/MarkTextInstall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-OWNER=$(stat -c '%U' $SCRIPTPATH)
-
-sudo -H -u $OWNER bash -c "flatpak install -y flathub com.github.marktext.marktext";
+flatpak install -y flathub com.github.marktext.marktext
 
 
