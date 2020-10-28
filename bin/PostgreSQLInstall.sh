f84e0647e77d9dc0efc6ec4c8db5b35177fbe293
Morten Eek <mortenee@gmail.com>
2020-10-28 14:22:24 +0100

    PostgreSQL updated to 12

--- bin/PostgreSQLInstall.sh
+++ bin/PostgreSQLInstall.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# WAIT: Bør bruke denne sjekken i tillegg til sjekk på om program er installert for å sikre at ikke duplikat entries i sources
-#isInFile=$(cat /etc/apt/sources.list | grep -c "http://apt.postgresql.org/pub/repos/apt/")
-#if [ $isInFile -eq 0 ]; then
-    #wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-    #echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/PostgreSQL.list
-    #apt-get update
-#fi
+apt-get install -y postgresql-12 postgresql-autodoc postgresql-plpython3-12;
 
-if [ $(dpkg-query -W -f='${Status}' postgresql-11 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -;
-    echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/PostgreSQL.list;
-    apt-get update;
-    apt-get install -y postgresql-11;
-    systemctl enable postgresql.service;
-    systemctl start postgresql.service;
-fi
+systemctl enable postgresql.service;
+systemctl start postgresql.service;
 
 sudo -H -u postgres bash -c 'psql --command "ALTER USER postgres PASSWORD '\''P@ssw0rd'\'';"';
-
 
