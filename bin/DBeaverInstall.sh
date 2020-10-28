201bb465cb3889871e252094e403fe88211d781e
Morten Eek <mortenee@gmail.com>
2020-10-28 12:39:21 +0100

    Improved DBeaver installer

--- bin/DBeaverInstall.sh
+++ bin/DBeaverInstall.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-if [ $(dpkg-query -W -f='${Status}' dbeaver-ce 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+isInFile=$(cat /etc/apt/sources.list.d/dbeaver.list | grep -c "https://dbeaver.io/debs/dbeaver-ce")
+if [ $isInFile -eq 0 ]; then
     wget --quiet -O - https://dbeaver.io/debs/dbeaver.gpg.key | apt-key add -;
     echo "deb https://dbeaver.io/debs/dbeaver-ce /" > /etc/apt/sources.list.d/dbeaver.list;
     apt-get update;
-    apt-get install -y dbeaver-ce;
 fi
 
-
-
-
+apt-get install -y dbeaver-ce;
 
 
