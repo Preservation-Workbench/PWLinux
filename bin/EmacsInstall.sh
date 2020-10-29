487fca112a09652a482e95471f8a38ed4b8b1d54
Morten Eek <mortenee@gmail.com>
2020-10-29 18:01:47 +0100

    Added emacs

---
+++ bin/EmacsInstall.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+isInFile=$(cat /etc/apt/sources.list.d/kelleyk-emacs-focal.list | grep -c "http://ppa.launchpad.net/kelleyk/emacs/ubuntu")
+if [ $isInFile -eq 0 ]; then
+    curl -sSL 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x873503a090750cdaeb0754d93ff0e01eeaafc9cd' | apt-key add -;
+    echo "deb http://ppa.launchpad.net/kelleyk/emacs/ubuntu focal main" > /etc/apt/sources.list.d/kelleyk-emacs-focal.list;
+fi
+
+apt-get update;
+apt-get install -y emacs27 libvterm-dev libtool-bin cmake;
+
+
+
+
+
+
+# SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+# OWNER=$(stat -c '%U' $SCRIPTPATH);
+
+# echo '#!/bin/bash
+# emacs --daemon' > /home/$OWNER/bin/emacs_daemon.sh;
+# chmod a+rx /home/$OWNER/bin/emacs_daemon.sh;
+# chown $OWNER:$OWNER /home/$OWNER/bin/emacs_daemon.sh;
+
+# echo "[Desktop Entry]
+# Type=Application
+# Exec=/home/$OWNER/bin/emacs_daemon.sh
+# Hidden=false
+# X-MATE-Autostart-enabled=true
+# Name=Emacs_Daemon" > /home/$OWNER/.config/autostart/Emacs_Daemon.desktop;
+# chown $OWNER:$OWNER /home/$OWNER/.config/autostart/Emacs_Daemon.desktop;
+
+# cat <<\EOF > /home/$OWNER/bin/amacs
+# #! /bin/bash -e
+# frame=`emacsclient -a '' -e "(member \"$DISPLAY\" (mapcar 'terminal-name (frames-on-display-list)))" 2>/dev/null`
+# [[ "$frame" == "nil" ]] && opts='-c' # if there is no frame open create one
+# [[ "${@/#-nw/}" == "$@" ]] && opts="$opts -n"
+# if [ -z "$@" ] ; then
+# 	wmctrl -xa emacs || emacsclient -n -c
+# else
+# 	exec emacsclient -a '' $opts "$@"
+# fi
+# wmctrl -xa emacs
+# EOF
+
+# chmod a+rx /home/$OWNER/bin/amacs;
+# chown $OWNER:$OWNER /home/$OWNER/bin/amacs;
+
+# if [ ! -f /home/$OWNER/.local/share/applications/emacs26.desktop ]; then
+#     cp /usr/share/applications/emacs26.desktop  /home/$OWNER/.local/share/applications;
+#     sed -i -e 's/emacs26 %F/amacs %F/' /home/$OWNER/.local/share/applications/emacs26.desktop;
+#     chown $OWNER:$OWNER /home/$OWNER/.local/share/applications/emacs26.desktop;
+# fi
+
+# REPOSRC="https://github.com/BBATools/Arkimacs.git"
+# LOCALREPO="/home/$OWNER/.emacs.d"
+# sudo -H -u $OWNER bash -c "git clone "$REPOSRC" "$LOCALREPO" 2> /dev/null || git -C "$LOCALREPO" pull"
+
+# #xdg-mime default codium.desktop text/english text/plain text/x-makefile text/x-c++hdr text/x-c++src text/x-chdr text/x-csrc text/x-java text/x-moc text/x-pascal text/x-tcl text/x-tex application/x-shellscript text/x-c text/x-c++
+
+
--- bin/SpotifyInstall.sh
+++ bin/SpotifyInstall.sh
@@ -2,8 +2,8 @@
 
 isInFile=$(cat /etc/apt/sources.list.d/spotify.list | grep -c "http://repository.spotify.com")
 if [ $isInFile -eq 0 ]; then
-    echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list;
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4773BD5E130D1D45;
+    echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list;
 fi
 
 apt-get update;
