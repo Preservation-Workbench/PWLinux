c6612bbe47c0fa2c39447f713fc16fd86319fec2
Morten Eek <mortenee@gmail.com>
2020-10-29 10:21:59 +0100

    Add mysql pwb user

--- bin/MySQLInstall.sh
+++ bin/MySQLInstall.sh
@@ -6,6 +6,7 @@ sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('P@ssw0rd');FLUSH PRIV
 sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 sudo mysql -e "DROP DATABASE test;DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';"
+sudo mysql -u root -psomething -e "CREATE USER 'pwb'@'localhost' IDENTIFIED BY 'P@ssw0rd';GRANT ALL PRIVILEGES ON *.* TO 'pwb'@'localhost';FLUSH PRIVILEGES;"
 
 sudo systemctl enable mysql;
 sudo systemctl start mysql;
