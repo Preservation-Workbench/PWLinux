875a4998407909e6c335b0ba30744443d1a66261
Morten Eek <mortenee@gmail.com>
2020-11-04 13:38:59 +0100

    Bundle wallpaper

--- bin/CorePostInstall.sh
+++ bin/CorePostInstall.sh
@@ -28,29 +28,11 @@ sudo apt-get install siegfried;
 
 # apt remove -y hexchat-common hexchat rhythmbox tomboy xplayer xfce4-taskmanager;
 
-# # snap install curl-simosx;
 
-# SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-# OWNER=$(stat -c '%U' $SCRIPTPATH);
-# # TODO: Flytt deler av dette til PWBInstall.sh
-# sudo -H -u $OWNER bash -c "python3 -m pip install -U ttkthemes unoconv zenipy execsql pdfy autopep8 rope toposort petl execsql epc JPype1==0.6.3 jaydebeapi --user";
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+OWNER=$(stat -c '%U' $SCRIPTPATH);
 
-# if [ $(dpkg-query -W -f='${Status}' ripgrep 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-#     wget -qO /tmp/ripgrep.deb https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb;
-#     apt-get install -y /tmp/ripgrep.deb;
-# fi
-
-# if [ ! -f /home/$OWNER/bin/xsv ]; then
-#     sudo -H -u $OWNER bash -c "wget -qO /home/$OWNER/bin/xsv.tar.gz https://github.com/BurntSushi/xsv/releases/download/0.13.0/xsv-0.13.0-x86_64-unknown-linux-musl.tar.gz";
-#     sudo -H -u $OWNER bash -c "cd /home/$OWNER/bin && dtrx xsv.tar.gz && rm xsv.tar.gz";
-# fi
-
-# if [ ! -f /home/$OWNER/bin/csvcleaner ]; then
-#     sudo -H -u $OWNER bash -c "wget -qO /home/$OWNER/bin/datatools.zip https://github.com/caltechlibrary/datatools/releases/download/v0.0.25/datatools-v0.0.25-linux-amd64.zip";
-#     sudo -H -u $OWNER bash -c "cd /home/$OWNER/bin && dtrx datatools.zip && rm datatools.zip";
-#     sudo -H -u $OWNER bash -c "cd /home/$OWNER/bin/datatools/bin && cp * ../../ && rm -rdf /home/$OWNER/bin/datatools";
-# fi
-
+# TODO: Test uten div-fiks først. Ikke sikkert trenger alt. Evt. sette noen i bashrc el heller
 # cat <<\EOF > /home/$OWNER/bin/div_fix.sh
 # #! /bin/bash
 # pkill gvfsd-fuse && /usr/lib/gvfs/gvfsd-fuse -o allow_other /var/run/user/1000/gvfs/
@@ -74,15 +56,15 @@ sudo apt-get install siegfried;
 # chown $OWNER:$OWNER /home/$OWNER/.config/autostart/Div_Fix.desktop;
 
 # # Set wallpaper
-# sudo -H -u $OWNER bash -c "mkdir -p /home/$OWNER/.local/share/wallpapers"
-# SRC_URI="https://raw.githubusercontent.com/Nitrux/luv-icon-theme/master/Wallpapers/Night/contents/images/2560x1080.png"
-# FNAME="/home/$OWNER/.local/share/wallpapers/arkimint.png"
-# if [ ! -f $FNAME ]; then
-#     sudo -H -u $OWNER bash -c "wget $SRC_URI -O $FNAME"
-# fi
-# USR_ID=$( id -u $OWNER )
-# export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$USR_ID/bus
-# su $OWNER -m -c "xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s $FNAME"
+sudo -H -u $OWNER bash -c "mkdir -p /home/$OWNER/.local/share/wallpapers"
+SRC=$SCRIPTPATH/img/pwlinux.png
+FNAME="/home/$OWNER/.local/share/wallpapers/pwlinux.png"
+if [ ! -f $FNAME ]; then
+    sudo -H -u $OWNER bash -c "wget $SRC -O $FNAME"
+fi
+USR_ID=$( id -u $OWNER )
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$USR_ID/bus
+su $OWNER -m -c "xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s $FNAME"
 
 # #TODO: Bruk mappe under fra PWB for å sjekke om er på Arkimint eller ikke
 # sudo -H -u $OWNER bash -c "mkdir -p /home/$OWNER/.arkimint";
