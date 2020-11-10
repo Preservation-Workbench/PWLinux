00813600be4175997e47b519ad32bb6c5339b0b3
Morten Eek <mortenee@gmail.com>
2020-11-10 10:04:49 +0100

    Add grip for markdown preview

--- bin/EmacsInstall.sh
+++ bin/EmacsInstall.sh
@@ -16,7 +16,7 @@ apt-get update;
 apt-get install -y emacs27 git libvterm-dev libtool-bin cmake ripgrep \
     hunspell-no hunspell fzf fd-find universal-ctags python3-bashate black \
     jq clang-format pipenv python3-pytest shellcheck node-js-beautify \
-    fonts-firacode direnv;
+    fonts-firacode direnv grip;
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 OWNER=$(stat -c '%U' $SCRIPTPATH);
