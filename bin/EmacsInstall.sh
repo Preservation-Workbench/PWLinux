8860406c6d130e08a7e5458f6140b966ad4c23f7
Morten Eek <mortenee@gmail.com>
2020-11-10 18:56:15 +0100

    Core + emacs install works

--- bin/EmacsInstall.sh
+++ bin/EmacsInstall.sh
@@ -26,9 +26,9 @@ REPOSRC="https://github.com/hlissner/doom-emacs"
 EMACSREPO="/home/$OWNER/.emacs.d"
 if [ ! -f "$EMACSREPO"/bin/doom ]; then
     sudo -H -u $OWNER bash -c "git clone --depth 1 "$REPOSRC" "$EMACSREPO";";
-    CMD="$EMACSREPO/bin/doom -y install --no-config --no-env --no-fonts;
-    sudo -H -u $OWNER bash -c "$CMD";
-    sudo -H -u $OWNER bash -c "$EMACSREPO/bin/doom env;";
+    sudo -H -u $OWNER bash -c ""$EMACSREPO"/bin/doom -y install --no-config \
+        --no-env --no-fonts;";
+    sudo -H -u $OWNER bash -c ""$EMACSREPO"/bin/doom env;";
 fi
 
 REPOSRC="https://github.com/Preservation-Workbench/PWEmacs"
@@ -47,15 +47,14 @@ fi
 
 sudo -H -u $OWNER bash -c ""$EMACSREPO"/bin/doom sync;";
 
-LPATH="/home/$OWNER/.local/share/applications"
-if [ ! -f $LPATH/emacs27.desktop ]; then
-    sudo -H -u $OWNER bash -c "mkdir -p $LPATH;";
-    CMD="cp /usr/share/applications/emacs27.desktop $LPATH;"
-    sudo -H -u $OWNER bash -c "$CMD";
+if [ ! -f /home/$OWNER/.local/share/applications/emacs27.desktop ]; then
+    sudo -H -u $OWNER bash -c "mkdir -p /home/$OWNER/.local/share/applications;";
+    sudo -H -u $OWNER bash -c "cp /usr/share/applications/emacs27.desktop \
+    /home/$OWNER/.local/share/applications/;";
     sed -i \
         '/Exec=emacs/c\Exec=sh -c "emacsclient -a emacs -n \"\$@\" || emacs" dummy %F'\
         /home/$OWNER/.local/share/applications/emacs27.desktop;  
-    chown $OWNER:$OWNER $LPATH/emacs27.desktop;
+    chown $OWNER:$OWNER /home/$OWNER/.local/share/applications/emacs27.desktop;
 fi
 
 isInFile=$(cat /home/$OWNER/.bashrc | grep -c "emacs()")
@@ -70,4 +69,3 @@ fi
 # text/x-c++hdr text/x-c++src text/x-chdr text/x-csrc text/x-java text/x-moc \
 # text/x-pascal text/x-tcl text/x-tex application/x-shellscript text/x-c \
 # text/x-c++
-
