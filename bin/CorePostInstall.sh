f35304733004656be0a40b44ec457db294363f5a
Morten Eek <mortenee@gmail.com>
2020-11-12 16:36:01 +0100

    Fixed dock install

--- bin/CorePostInstall.sh
+++ bin/CorePostInstall.sh
@@ -110,7 +110,8 @@ source $SCRIPTPATH/EmacsInstall.sh
 # Install Oracle:
 source $SCRIPTPATH/OracleInstall.sh
 isInFile=$(cat /etc/lightdm/lightdm.conf | grep -c "greeter-hide-users=true")
-if [ $isInFile -eq 0 ]; then    
+
+if [ $isInFile -eq 0 ]; then
     echo 'greeter-hide-users=true' >> /etc/lightdm/lightdm.conf; #Hide oracle user
 fi
 
@@ -128,23 +129,20 @@ source $SCRIPTPATH/PostgreSQLInstall.sh
 source $SCRIPTPATH/OnlyofficeInstall.sh
 
 # Configure Xfce panel:
-sudo add-apt-repository ppa:xubuntu-dev/extras;
+sudo add-apt-repository -y ppa:xubuntu-dev/extras;
 sudo apt-get update;
 apt-get install -y  xfce4-docklike-plugin;
+
+su $OWNER -m -c "cat <<\EOF > /home/$OWNER/.config/xfce4/panel/docklike-100.rc
+[user]
+pinned=/usr/share/applications/xfce4-terminal.desktop;/home/$OWNER/.local/share/applications/emacs27.desktop;/usr/share/applications/thunar.desktop;/usr/share/applications/chromium.desktop;
+EOF
+"
 
 sudo -H -u $OWNER bash -c "rm -rdf /home/$OWNER/.config/xfce4/panel/launcher*"
 su $OWNER -m -c "xfconf-query -c xfce4-panel -pn "/plugins/plugin-100" -t string -s 'docklike'"
 su $OWNER -m -c "xfconf-query -c xfce4-panel -p /panels/panel-1/plugin-ids -n -a -t int -s 1 -t int -s 2 -t int -s 100 -t int -s 7 -t int -s 8 -t int -s 9 -t int -s 10 -t int -s 11 -t int -s 12 -t int -s 13"
+su $OWNER -m -c "xfconf-query --channel 'xfce4-panel' --property '/panels/panel-1/size' --type int --set 40"
 su $OWNER -m -c "xfce4-panel -r "
 
-cat <<\EOF > /home/$OWNER /.config/xfce4/panel/docklike-100.rc
-[user]
-pinned=/usr/share/applications/xfce4-terminal.desktop;/home/$OWNER/.local/share/applications/emacs27.desktop;/usr/share/applications/thunar.desktop;/usr/share/applications/chromium.desktop;
-EOF
 
-
-
-
-#I fil /home/$OWNER /.config/xfce4/panel/docklike-100.rc
-#[user]
-#pinned=/usr/share/applications/xfce4-terminal.desktop;/home/pwb/.local/share/applications/emacs27.desktop;/usr/share/applications/thunar.desktop;/usr/share/applications/chromium.desktop;
