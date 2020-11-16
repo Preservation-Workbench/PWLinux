5fadac5578c93e5ff40702fd28d6305194417875
Morten Eek <mortenee@gmail.com>
2020-11-16 17:16:15 +0100

    Take 2

--- bin/MSSQLInstall.sh
+++ bin/MSSQLInstall.sh
@@ -40,7 +40,7 @@ if [ -f "/opt/mssql/bin/mssql-conf" ]; then
     export MSSQL_PID="Express"
     export MSSQL_SA_PASSWORD="P@ssw0rd"
     /opt/mssql/bin/mssql-conf -n setup accept-eula;
-    echo "$OWNER ALL = (root) NOPASSWD:  /bin/systemctl start mssql-server.service" > /etc/sudoers.d/mssql;
+    echo "$OWNER ALL = (root) NOPASSWD: /bin/systemctl start mssql-server.service" > /etc/sudoers.d/mssql;
     sudo chmod 0440 /etc/sudoers.d/mssql;  
 fi
 
