9ab3c3cccc9af56ea8c92ca429c635c19fafb5f5
Morten Eek <mortenee@gmail.com>
2020-11-16 13:34:03 +0100

    Path 2

--- bin/MSSQLInstall.sh
+++ bin/MSSQLInstall.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 killall synaptic;
 
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+OWNER=$(stat -c '%U' $SCRIPTPATH);
+PWCONFIGDIR=/home/$OWNER/.config/pwlinux
+
 if [ ! -f /tmp/microsoft.gpg ]; then
     cd /tmp && curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
     install -o root -g root -m 644 /tmp/microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg;
@@ -26,7 +30,6 @@ ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev freetds-dev freetds-bi
 
 # Workaround for recurring index hash error on package server:
 if [ $(dpkg-query -W -f='${Status}' mssql-server 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    PWCONFIGDIR=/home/$OWNER/.config/pwlinux
     sudo -H -u $OWNER bash -c "mkdir -p $PWCONFIGDIR";
     apt-get install -y libc++1 libc++abi1 libsasl2-modules-gssapi-mit libsss-nss-idmap0;    
     cd $PWCONFIGDIR && wget -c https://packages.microsoft.com/ubuntu/18.04/mssql-server-2019/pool/main/m/mssql-server/mssql-server_15.0.4073.23-4_amd64.deb
