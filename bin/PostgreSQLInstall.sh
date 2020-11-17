a4749eeb96a21bf8cc1a15c451e70e567667572d
Morten Eek <mortenee@gmail.com>
2020-11-17 15:25:09 +0100

    MySQL user fix

--- bin/PostgreSQLInstall.sh
+++ bin/PostgreSQLInstall.sh
@@ -10,4 +10,7 @@ systemctl start postgresql.service;
 sudo -i -u postgres bash -c \
     'psql --command "ALTER USER postgres PASSWORD '\''P@ssw0rd'\'';"';
 
+# TODO: Lag tilsvarende som for mssql så bruker kan starte/stoppe alle databaser
+# echo "$OWNER ALL=(ALL) NOPASSWD: /bin/systemctl start mssql-server.service,/bin/systemctl stop mssql-server.service" > /etc/sudoers.d/mssql;
+# sudo chmod 0440 /etc/sudoers.d/mssql;  
 
