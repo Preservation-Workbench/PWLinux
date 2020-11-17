bce6dc3e172c517940d3a3675d702518948e46be
Morten Eek <mortenee@gmail.com>
2020-11-17 18:18:35 +0100

    Improved mysql installer

--- bin/MySQLInstall.sh
+++ bin/MySQLInstall.sh
@@ -6,41 +6,45 @@ apt-get install -y mysql-server-8.0 expect;
 
 MYSQL_ROOT_PASSWORD='P@ssw0rd'
 MYSQL=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $11}')
-expect -c "
-    set timeout 10
-    spawn mysql_secure_installation
+USER_EXISTS="$(mysql -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'pwb')")"
+if [ "$USER_EXISTS" -ne 1 ]; then
+    expect -c "
+        set timeout 10
+        spawn mysql_secure_installation
 
-    expect \"Enter password for user root:\"
-    send \"$MYSQL\r\"
-    expect \"New password:\"
-    send \"$MYSQL_ROOT_PASSWORD\r\"
-    expect \"Re-enter new password:\"
-    send \"$MYSQL_ROOT_PASSWORD\r\"
-    expect \"Change the password for root ?\ ((Press y\|Y for Yes, any other key for No) :\"
-    send \"y\r\"
-    send \"$MYSQL\r\"
-    expect \"New password:\"
-    send \"$MYSQL_ROOT_PASSWORD\r\"
-    expect \"Re-enter new password:\"
-    send \"$MYSQL_ROOT_PASSWORD\r\"
-    expect \"Do you wish to continue with the password provided?\(Press y\|Y for Yes, any other key for No) :\"
-    send \"y\r\"
-    expect \"Remove anonymous users?\(Press y\|Y for Yes, any other key for No) :\"
-    send \"y\r\"
-    expect \"Disallow root login remotely?\(Press y\|Y for Yes, any other key for No) :\"
-    send \"n\r\"
-    expect \"Remove test database and access to it?\(Press y\|Y for Yes, any other key for No) :\"
-    send \"y\r\"
-    expect \"Reload privilege tables now?\(Press y\|Y for Yes, any other key for No) :\"
-    send \"y\r\"
-    expect eof
-    "
+        expect \"Enter password for user root:\"
+        send \"$MYSQL\r\"
+        expect \"New password:\"
+        send \"$MYSQL_ROOT_PASSWORD\r\"
+        expect \"Re-enter new password:\"
+        send \"$MYSQL_ROOT_PASSWORD\r\"
+        expect \"Change the password for root ?\ ((Press y\|Y for Yes, any other key for No) :\"
+        send \"y\r\"
+        send \"$MYSQL\r\"
+        expect \"New password:\"
+        send \"$MYSQL_ROOT_PASSWORD\r\"
+        expect \"Re-enter new password:\"
+        send \"$MYSQL_ROOT_PASSWORD\r\"
+        expect \"Do you wish to continue with the password provided?\(Press y\|Y for Yes, any other key for No) :\"
+        send \"y\r\"
+        expect \"Remove anonymous users?\(Press y\|Y for Yes, any other key for No) :\"
+        send \"y\r\"
+        expect \"Disallow root login remotely?\(Press y\|Y for Yes, any other key for No) :\"
+        send \"n\r\"
+        expect \"Remove test database and access to it?\(Press y\|Y for Yes, any other key for No) :\"
+        send \"y\r\"
+        expect \"Reload privilege tables now?\(Press y\|Y for Yes, any other key for No) :\"
+        send \"y\r\"
+        expect eof
+        "
 
-mysql -e "CREATE USER IF NOT EXISTS 'pwb'@'localhost' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';"
-mysql -e "GRANT ALL ON *.* TO 'pwb'@'localhost' WITH GRANT OPTION;FLUSH PRIVILEGES;"
+    mysql -e "CREATE USER IF NOT EXISTS 'pwb'@'localhost' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';"
+    mysql -e "GRANT ALL ON *.* TO 'pwb'@'localhost' WITH GRANT OPTION;FLUSH PRIVILEGES;"        
+fi
 
-# TODO: Lag tilsvarende som for mssql så bruker kan starte/stoppe alle databaser
-# echo "$OWNER ALL=(ALL) NOPASSWD: /bin/systemctl start mssql-server.service,/bin/systemctl stop mssql-server.service" > /etc/sudoers.d/mssql;
-# sudo chmod 0440 /etc/sudoers.d/mssql;  
+echo "$OWNER ALL=(ALL) NOPASSWD: /bin/systemctl start mysql,/bin/systemctl stop mysql" > /etc/sudoers.d/mysql;
+sudo chmod 0440 /etc/sudoers.d/myssql;  
+systemctl disable mysql; 
+
 
 
