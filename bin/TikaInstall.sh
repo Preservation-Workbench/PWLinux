a4cb86dbcbaf7c58e60f12edcf5786213821bc9d
Morten Eek <mortenee@gmail.com>
2020-11-24 15:08:51 +0100

    Apache Tika installer

--- bin/TikaInstall.sh
+++ bin/TikaInstall.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-
-#TODO: Lag desktop-fil/script for å åpne tika-app fra meny
-
 SCRIPTPATH=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
 OWNER=$(stat -c '%U' $SCRIPTPATH)
+PWCONFIGDIR=/home/$OWNER/.config/pwlinux
+APPS=/home/$OWNER/.local/share/applications
 ver="1.24.1"
-tika_path="/home/$OWNER/bin/tika"
+$TIKAPATH="/home/$OWNER/bin/tika"
 
-if [ ! -f $tika_path/tika-app.jar ]; then
-    sudo -H -u $OWNER bash -c "mkdir -p /home/$OWNER/bin/tika; \
-    curl -o /home/$OWNER/bin/tika/tika-app.jar https://archive.apache.org/dist/tika/tika-app-${ver}.jar;";    
+if [ ! -f $TIKAPATH/tika-app.jar ]; then
+    sudo -H -u $OWNER bash -c "mkdir -p $TIKAPATH; \
+    curl -o $TIKAPATH/tika-app.jar https://archive.apache.org/dist/tika/tika-app-${ver}.jar;";    
     # curl -o /home/$OWNER/bin/tika/tika-app.jar --doh-url https://1.1.1.1/dns-query https://archive.apache.org/dist/tika/tika-app-${ver}.jar;";
+    sudo -H -u $OWNER bash -c "cp $SCRIPTPATH/data/tika/tika.config $TIKAPATH;";
+fi
+
+SRC=$SCRIPTPATH/img/apache_icon.png
+FNAME="$PWCONFIGDIR/img/apache.png"
+if [ ! -f $FNAME ]; then
+    sudo -H -u $OWNER bash -c "cp $SRC $FNAME"
+fi
+
+if [ ! -f $APPS/tika.desktop ]; then
+    sudo -H -u $OWNER bash -c "mkdir -p $APPS;";
+    sudo -H -u $OWNER bash -c "cp $SCRIPTPATH/desktop/tika.desktop $APPS;";
+    sed -i "/Exec=dummy/c\Exec=/usr/bin/java -jar /home/$OWNER/bin/tika/tika-app.jar" $APPS/Tika.desktop; 
+    sed -i "/Icon=dummy/c\Icon=$FNAME" $APPS/Tika.desktop;   
+    chown $OWNER:$OWNER $APPS/Tika.desktop; ;    
 fi
 
 cd $SCRIPTPATH;
