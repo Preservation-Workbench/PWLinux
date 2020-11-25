8969f742804502a1d4799ced9401b7524444cdda
Morten Eek <mortenee@gmail.com>
2020-11-25 10:30:26 +0100

    Path fix

--- bin/VeraInstall.sh
+++ bin/VeraInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPTPATH=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
 OWNER=$(stat -c '%U' $SCRIPTPATH);
-VERADIR=/home/$OWNER/bin/verapdf/verapdf 
+VERADIR=/home/$OWNER/bin/verapdf
 
 # WAIT: Lag menyvalg + kopier eller lenke executables til path
 
