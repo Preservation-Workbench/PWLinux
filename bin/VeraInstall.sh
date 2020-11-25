a110cafdf2591649771f210a9b6605cf0a4bd1c2
Morten Eek <mortenee@gmail.com>
2020-11-25 10:27:39 +0100

    sed in xml is hard

--- bin/VeraInstall.sh
+++ bin/VeraInstall.sh
@@ -7,15 +7,16 @@ VERADIR=/home/$OWNER/bin/verapdf/verapdf
 
 if [ ! -f $VERADIR ]; then
     rm -rdf /tmp/verapdf*
+    cp $SCRIPTPATH/data/verapdf/auto.xml /tmp/auto.xml;
+    sed -i "s#<installpath></installpath>#<installpath>$VERADIR</installpath>#" /tmp/auto.xml;
     sudo -H -u $OWNER bash -c '
     mkdir -p '"$VERADIR"';
     wget -O /tmp/verapdf-installer.zip http://downloads.verapdf.org/rel/verapdf-installer.zip
     unzip /tmp/verapdf-installer.zip -d /tmp/verapdf;
-    cp '"$SCRIPTPATH"'/data/verapdf/auto.xml /tmp/auto.xml;
-    sed -i "/<installpath></installpath>/c\<installpath>'"$VERADIR"'</installpath>" /tmp/auto.xml; 
-    java -jar /tmp/verapdf/verapdf-greenfield-*/verapdf-izpack-installer-* '"$SCRIPTPATH"'/data/verapdf/auto.xml;
+    java -jar /tmp/verapdf/verapdf-greenfield-*/verapdf-izpack-installer-* '"$SCRIPTPATH"' /tmp/auto.xml;
     ln -s '"$VERADIR"' /home/'"$OWNER"'/bin/verapdf.sh;'
 fi
 
 cd $SCRIPTPATH;
 
+#sed -i "/<installpath></installpath>/c\<installpath>'"$VERADIR"'</installpath>" /tmp/auto.xml; 
