83b28add3a1cdd2103bc52066f05a7b51d85a9f5
Morten Eek <mortenee@gmail.com>
2020-11-30 17:00:24 +0100

    Improved siard installer

--- bin/SiardSuiteInstall.sh
+++ bin/SiardSuiteInstall.sh
@@ -3,9 +3,12 @@ SCRIPTPATH=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
 OWNER=$(stat -c '%U' $SCRIPTPATH);
 APPS=/home/$OWNER/.local/share/applications
 PWCONFIGDIR=/home/$OWNER/.config/pwlinux
-URL=https://github.com/sfa-siard/SiardGui/releases/download/2.1.34/SIARD-Suite-2.1.134.zip
+VER="2.1.134"
+URL=https://github.com/sfa-siard/SiardGui/releases/download/${VER}/SIARD-Suite-${VER}.zip
 BINDIR=/home/$OWNER/bin
 SIARDDIR=$BINDIR/sfa-siard
+
+# TODO: For dbptk cli bruk: /usr/lib/jvm/bellsoft-java8-runtime-full-amd64/bin/java -jar dbptk-app-2.9.6.jar 
 
 isInFile=$(cat /etc/apt/sources.list.d/bellsoft.list | grep -c "https://apt.bell-sw.com/")
 if [ $isInFile -eq 0 ]; then    
@@ -18,6 +21,8 @@ apt-get install bellsoft-java8-runtime-full;
 
 if [ ! -f $SIARDDIR/siardgui.sh ]; then
     sudo -H -u $OWNER bash -c "mkdir -p $SIARDDIR;";
+    sudo -H -u $OWNER bash -c "mkdir -p /home/$OWNER/.java;";
+    sudo -H -u $OWNER bash -c "echo "\""installed.version=${VER}"\"" > /home/$OWNER/.java/siard_suite_2.1.properties;";
     sudo -H -u $OWNER bash -c "wget -O /tmp/SIARD-Suite.zip $URL";
     sudo -H -u $OWNER bash -c "unzip /tmp/SIARD-Suite.zip -d $BINDIR;";
     sudo -H -u $OWNER bash -c "mv -v $BINDIR/siard_suite-*/* $SIARDDIR;";
@@ -30,18 +35,12 @@ if [ ! -f $BINDIR/siardgui.sh ]; then
     sudo -H -u $OWNER bash -c "cp $SRC $BINDIR/siardgui.sh";
 fi
 
-SRC=$SCRIPTPATH/img/siardsuite.png
-FNAME=$PWCONFIGDIR/img/siardsuite.png
-if [ ! -f $FNAME ]; then
-    sudo -H -u $OWNER bash -c "mkdir -p $PWCONFIGDIR/img;";
-    sudo -H -u $OWNER bash -c "cp $SRC $FNAME"
-fi
-
+ICON=$SIARDDIR/doc/manual/siardgui.png
 if [ ! -f $APPS/siardsuite.desktop ]; then
     sudo -H -u $OWNER bash -c "mkdir -p $APPS;";
     sudo -H -u $OWNER bash -c "cp $SCRIPTPATH/desktop/siardsuite.desktop $APPS;";
     sed -i "/Exec=dummy/c\Exec=$BINDIR/siardgui.sh" $APPS/siardsuite.desktop; 
-    sed -i "/Icon=dummy/c\Icon=$FNAME" $APPS/siardsuite.desktop;   
+    sed -i "/Icon=dummy/c\Icon=$ICON" $APPS/siardsuite.desktop;   
     chown $OWNER:$OWNER $APPS/siardsuite.desktop;  
 fi
 
--- bin/TikaInstall.sh
+++ bin/TikaInstall.sh
@@ -3,13 +3,13 @@ SCRIPTPATH=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
 OWNER=$(stat -c '%U' $SCRIPTPATH)
 PWCONFIGDIR=/home/$OWNER/.config/pwlinux
 APPS=/home/$OWNER/.local/share/applications
-ver="1.24.1"
+VER="1.24.1"
 TIKAPATH=/home/$OWNER/bin/tika
 
 if [ ! -f $TIKAPATH/tika-app.jar ]; then
     sudo -H -u $OWNER bash -c "mkdir -p $TIKAPATH; \
-    curl -o $TIKAPATH/tika-app.jar https://archive.apache.org/dist/tika/tika-app-${ver}.jar;";    
-    # curl -o /home/$OWNER/bin/tika/tika-app.jar --doh-url https://1.1.1.1/dns-query https://archive.apache.org/dist/tika/tika-app-${ver}.jar;";
+    curl -o $TIKAPATH/tika-app.jar https://archive.apache.org/dist/tika/tika-app-${VER}.jar;";    
+    # curl -o /home/$OWNER/bin/tika/tika-app.jar --doh-url https://1.1.1.1/dns-query https://archive.apache.org/dist/tika/tika-app-${VER}.jar;";
     sudo -H -u $OWNER bash -c "cp $SCRIPTPATH/data/tika/tika.config $TIKAPATH;";
 fi
 
