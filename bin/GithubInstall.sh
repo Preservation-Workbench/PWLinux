f720dcef6908ae3021b72d0dc54b1d6d7c0d0946
Morten Eek <mortenee@gmail.com>
2020-12-01 09:45:14 +0100

    Add wmclass to desktop file

--- bin/GithubInstall.sh
+++ bin/GithubInstall.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+SCRIPTPATH=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
+OWNER=$(stat -c '%U' $SCRIPTPATH);
+APPS=/home/$OWNER/.local/share/applications
+
 isInFile=$(cat /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list | grep -c "https://packagecloud.io/shiftkey/desktop/any/")
 if [ $isInFile -eq 0 ]; then    
     wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
@@ -7,3 +11,16 @@ if [ $isInFile -eq 0 ]; then
 fi
 
 apt-get install -y github-desktop;
+
+if [ ! -f /home/$OWNER/.local/share/applications/github-desktop.desktop ]; then
+    sudo -H -u $OWNER bash -c "mkdir -p $APPS;";
+    sudo -H -u $OWNER bash -c "cp /usr/share/applications/github-desktop.desktop $APPS;";
+fi
+
+WMCLASS='StartupWMClass=GitHub Desktop'
+isInFile=$(cat $APPS/github-desktop.desktop | grep -c $WMCLASS)
+if [ $isInFile -eq 0 ]; then 
+    sudo -H -u $OWNER bash -c "echo $WMCLASS >> $APPS/github-desktop.desktop;";
+fi
+
+cd $SCRIPTPATH;
