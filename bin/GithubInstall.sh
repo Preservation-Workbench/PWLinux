6f155fd196ef0e9c4a069a28dad74df884626f3a
Morten Eek <mortenee@gmail.com>
2020-12-01 09:33:20 +0100

    github installer fix

--- bin/GithubInstall.sh
+++ bin/GithubInstall.sh
@@ -2,7 +2,7 @@
 isInFile=$(cat /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list | grep -c "https://packagecloud.io/shiftkey/desktop/any/")
 if [ $isInFile -eq 0 ]; then    
     wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
-    echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main' > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list;
+    echo 'deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main' > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list;
     apt-get update;
 fi
 
