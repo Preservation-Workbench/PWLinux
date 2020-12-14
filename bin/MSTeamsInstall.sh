29334e857254e776e9843a0a53d851da7e4a674b
Morten Eek <mortenee@gmail.com>
2020-12-14 09:52:00 +0100

    Teams mic fix

--- bin/MSTeamsInstall.sh
+++ bin/MSTeamsInstall.sh
@@ -5,6 +5,8 @@ OWNER=$(stat -c '%U' $SCRIPTPATH);
 if [ ! -f /tmp/microsoft.gpg ]; then
     cd /tmp && curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
     install -o root -g root -m 644 /tmp/microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg;
+    # Fix for mic:
+    usermod -a -G pulse,audio $OWNER
 fi
 
 isInFile=$(cat /etc/apt/sources.list.d/teams.list | grep -c "https://packages.microsoft.com/repos/ms-teams")
