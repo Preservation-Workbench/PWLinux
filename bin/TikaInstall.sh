83fad762aa553e282f24354a4a00781f7c39f5e7
Morten Eek <mortenee@gmail.com>
2021-03-25 10:09:10 +0100

    Update Tika because of bug

--- bin/TikaInstall.sh
+++ bin/TikaInstall.sh
@@ -6,9 +6,13 @@ APPS=/home/$OWNER/.local/share/applications
 VER="1.24.1"
 TIKAPATH=/home/$OWNER/bin/tika
 
+# TODO: Change to 2.0 stable release when available
+# Using alpha for now because of bug when parsing config file in alle previous versions 
+
 if [ ! -f $TIKAPATH/tika-app.jar ]; then
     sudo -H -u $OWNER bash -c "mkdir -p $TIKAPATH; \
-    curl -o $TIKAPATH/tika-app.jar https://archive.apache.org/dist/tika/tika-app-${VER}.jar;";    
+    curl -o $TIKAPATH/tika-app.jar https://archive.apache.org/dist/tika/tika-app-2.0.0-ALPHA.jar;";  
+    # curl -o $TIKAPATH/tika-app.jar https://archive.apache.org/dist/tika/tika-app-${VER}.jar;";    
     # curl -o /home/$OWNER/bin/tika/tika-app.jar --doh-url https://1.1.1.1/dns-query https://archive.apache.org/dist/tika/tika-app-${VER}.jar;";
     sudo -H -u $OWNER bash -c "cp $SCRIPTPATH/data/tika/tika.config $TIKAPATH;";
 fi
