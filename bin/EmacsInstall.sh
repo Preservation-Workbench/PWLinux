d64e6e889bd79a89ba094e722511d6b5ac087fec
Morten Eek <mortenee@gmail.com>
2022-01-11 14:44:15 +0100

    Use emacs-ng

--- bin/EmacsInstall.sh
+++ bin/EmacsInstall.sh
@@ -4,20 +4,27 @@ OWNER=$(stat -c '%U' $SCRIPTPATH);
 
 # TODO: Legg inn default xresources fil
 
-url="https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x873503a090750cda\
-    eb0754d93ff0e01eeaafc9cd"
-url=$(echo $url | tr -d ' ')
-echo $url
-isInFile=$(cat /etc/apt/sources.list.d/kelleyk-emacs-focal.list | grep -c \
-    "http://ppa.launchpad.net/kelleyk/emacs/ubuntu")
-if [ $isInFile -eq 0 ]; then
-    curl -sSL "$url" | apt-key add -;
-    echo "deb http://ppa.launchpad.net/kelleyk/emacs/ubuntu focal main"\
-        > /etc/apt/sources.list.d/kelleyk-emacs-focal.list;
-fi
+# url="https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x873503a090750cda\
+#     eb0754d93ff0e01eeaafc9cd"
+# url=$(echo $url | tr -d ' ')
+# echo $url
+# isInFile=$(cat /etc/apt/sources.list.d/kelleyk-emacs-focal.list | grep -c \
+#     "http://ppa.launchpad.net/kelleyk/emacs/ubuntu")
+# if [ $isInFile -eq 0 ]; then
+#     curl -sSL "$url" | apt-key add -;
+#     echo "deb http://ppa.launchpad.net/kelleyk/emacs/ubuntu focal main"\
+#         > /etc/apt/sources.list.d/kelleyk-emacs-focal.list;
+# fi
+
+
+if [ $(dpkg-query -W -f='${Status}' emacs-ng 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    apt-get install -y libncurses5 libgccjit0 libjpeg9;  
+    cd /tmp/ && wget -c https://github.com/emacs-ng/emacs-ng/releases/download/v0.0.7a111cc/emacs-ng_0.0.7a111cc.webrender_amd64.deb
+    sudo DEBIAN_FRONTEND=noninteractive dpkg --install emacs-ng_0.0.7a111cc.webrender_amd64.deb
+fi 
 
 apt-get update;
-apt-get install -y emacs27 git libvterm-dev libtool-bin cmake ripgrep \
+apt-get install -y git libvterm-dev libtool-bin cmake ripgrep \
     hunspell-no hunspell universal-ctags python3-bashate black \
     jq clang-format pipenv python3-pytest shellcheck node-js-beautify \
     fonts-firacode direnv grip;
@@ -48,15 +55,15 @@ fi
 
 sudo -H -u $OWNER bash -c ""$EMACSREPO"/bin/doom sync;";
 
-if [ ! -f /home/$OWNER/.local/share/applications/emacs27.desktop ]; then
-    sudo -H -u $OWNER bash -c "mkdir -p /home/$OWNER/.local/share/applications;";
-    sudo -H -u $OWNER bash -c "cp /usr/share/applications/emacs27.desktop \
-    /home/$OWNER/.local/share/applications/;";
-    sed -i \
-        '/Exec=emacs/c\Exec=sh -c "emacsclient -a emacs -n \"\$@\" || emacs" dummy %F'\
-        /home/$OWNER/.local/share/applications/emacs27.desktop;  
-    chown $OWNER:$OWNER /home/$OWNER/.local/share/applications/emacs27.desktop;
-fi
+# if [ ! -f /home/$OWNER/.local/share/applications/emacs27.desktop ]; then
+#     sudo -H -u $OWNER bash -c "mkdir -p /home/$OWNER/.local/share/applications;";
+#     sudo -H -u $OWNER bash -c "cp /usr/share/applications/emacs27.desktop \
+#     /home/$OWNER/.local/share/applications/;";
+#     sed -i \
+#         '/Exec=emacs/c\Exec=sh -c "emacsclient -a emacs -n \"\$@\" || emacs" dummy %F'\
+#         /home/$OWNER/.local/share/applications/emacs27.desktop;  
+#     chown $OWNER:$OWNER /home/$OWNER/.local/share/applications/emacs27.desktop;
+# fi
 
 isInFile=$(cat /home/$OWNER/.bashrc | grep -c "emacs()")
 if [ $isInFile -eq 0 ]; then
