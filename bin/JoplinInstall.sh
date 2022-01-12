a92198b56d9dd7222a95bdcba49bd98aae07adb1
Morten Eek <mortenee@gmail.com>
2022-01-12 11:15:00 +0100

    Use flatpak for Joplin

--- bin/JoplinInstall.sh
+++ bin/JoplinInstall.sh
@@ -3,6 +3,8 @@
 SCRIPTPATH=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
 OWNER=$(stat -c '%U' $SCRIPTPATH);
 
-sudo -H -u $OWNER bash -c "export XDG_CURRENT_DESKTOP=XFCE ; export TERM=xterm ; wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash"
-cd $SCRIPTPATH;
+# sudo -H -u $OWNER bash -c "export XDG_CURRENT_DESKTOP=XFCE ; export TERM=xterm ; wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash"
 
+flatpak install -y --noninteractive flathub net.cozic.joplin_desktop;
+
+cd $SCRIPTPATH;
